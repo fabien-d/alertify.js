d4bb9c65decee409e65ae1ca124b2256962aa2eb
Fabien Doiron <fabien.doiron@gmail.com>
2012-06-09 18:18:30 -0400

    Major overhaul.. re-factored into a singleton. Still very much a work in progress

--- debug/whatup.js
+++ debug/whatup.js
@@ -2,141 +2,163 @@
 	"use strict";
 
 	var document = global.document,
-	    // Methods
-	    WhatUp,
-	    buildDialogs,
-	    showDialog,
-	    hideDialog,
-	    setEvents,
-	    // Properties
-	    callback,
-	    element,
-	    dialog,
-	    cover,
-	    alertHTML   = "",
-	    confirmHTML = "",
-	    logHTML     = "",
-	    promptHTML  = "";
+	    WhatUp;
 
-	/**
-	 * Initialize WhatUp
-	 * Create the 2 main elements
-	 */
-	WhatUp = function () {
-		// cover
-		cover = document.createElement("div");
-		cover.setAttribute("id", "whatupcover");
-		cover.className = "wu-cover wu-hidden";
-		document.body.appendChild(cover);
-		// main element
-		element = document.createElement("section");
-		element.setAttribute("id", "whatup");
-		element.className = "whatup wu-hidden";
-		document.body.appendChild(element);
+	WhatUp = (function () {
 
-		buildDialogs();
-	};
+		var init, build, hide, alert, confirm, log, prompt,
+		    cover, dialogs, element,
 
-	/**
-	 * Store the various elements into variables for easy access
-	 */
-	buildDialogs = function () {
-		var btnOK     = "<a class=\"wu-button wu-button-ok\" id=\"wuOK\">OK</a>",
-		    btnCancel = "<a class=\"wu-button wu-button-cancel\" id=\"wuCancel\">Cancel</a>";
-		
-		dialog        = "<div class=\"wu-dialog\">{{type}}</div>";
-		alertHTML     = "<article class=\"wu-inner wu-alert\">" +
-		                	"<p>{{message}}</p>" +
-		                	"<nav class=\"wu-buttons\">" +
-		                		btnOK +
-		                	"</nav>" +
-		                "</article>";
-		confirmHTML   = "<article class=\"wu-inner wu-confirm\">" +
-		                	"<p>{{message}}</p>" +
-		                	"<nav class=\"wu-buttons\">" +
-		                	    btnCancel + btnOK +
-		                	"</nav>" +
-		                "</article>";
-		logHTML       = "<article class=\"wu-inner wu-confirm\">" +
-		                	"<p>{{message}}</p>" +
-		                "</article>";
-		promptHTML    = "<article class=\"wu-inner wu-prompt\">" +
-		                	"<p>{{message}}</p>" +
-		                	"<input type=\"text\" class=\"wu-text\">" +
-		                	"<nav class=\"wu-buttons\">" +
-		                	    btnCancel + btnOK +
-		                	"</nav>" +
-		                "</article>";
-	};
+		    setEvents,
+		    callback;
 
-	/**
-	 * Close the dialog box and reset the needs properties
-	 */
-	hideDialog = function () {
-		callback = undefined;
-		element.className = "whatup wu-hidden";
-		cover.className   = "wu-cover wu-hidden";
-		element.innerHTML = "";
+		dialogs = {
+			buttons : {
+				holder : "<nav class=\"wu-buttons\">{{buttons}}</nav>",
+				ok     : "<a class=\"wu-button wu-button-ok\" id=\"wuOK\">OK</a>",
+				cancel : "<a class=\"wu-button wu-button-cancel\" id=\"wuCancel\">Cancel</a>"
+			},
+			input   : "<input type=\"text\" class=\"wu-text\">",
+			message : "<p class=\"wu-message\">{{message}}</p>"
+		};
 
-		document.body.style.overflow = "visible";
-	};
+		/**
+		 * Initialize WhatUp
+		 * Create the 2 main elements
+		 */
+		init = function () {
+			// cover
+			cover = document.createElement("div");
+			cover.setAttribute("id", "whatupcover");
+			cover.className = "wu-cover wu-hidden";
+			document.body.appendChild(cover);
+			// main element
+			element = document.createElement("section");
+			element.setAttribute("id", "whatup");
+			element.className = "whatup wu-hidden";
+			document.body.appendChild(element);
+		};
 
-	/**
-	 * Build an show the proper dialog box
-	 * 
-	 * @param  {String} message The message to be displayed, passed from the function call
-	 * @param  {String} html    The html string to use to populate the dialog
-	 */
-	showDialog = function (message, html) {
-		element.className = "whatup";
-		element.innerHTML = dialog.replace("{{type}}", html.replace("{{message}}", message));
-		document.body.style.overflow = "hidden";
+		/**
+		 * Build the proper message box
+		 * 
+		 * @param  {String} type    The type of message box to build
+		 * @param  {String} message The message passed from the function
+		 * @return {String}         An HTML string of the message box
+		 */
+		build = function (type, message) {
+			var html = "";
 
-		setEvents();
-	};
+			html += "<div class=\"wu-dialog\">";
+			html += 	"<article class=\"wu-inner\">";
+			html += 		dialogs.message.replace("{{message}}", message);
 
-	/**
-	 * Set the proper button click events
-	 */
-	setEvents = function () {
-		var btnOK     = document.getElementById("wuOK") || undefined,
-		    btnCancel = document.getElementById("wuCancel") || undefined;
+			if (type === "prompt") { html += dialogs.input; }
 
-		if (typeof btnOK !== "undefined") {
-			btnOK.addEventListener("click", function () {
-				if (typeof callback === "function") { callback(true); }
-				hideDialog();
-			}, false);
-		}
+			html += 		dialogs.buttons.holder;
+			html += 	"</article>";
+			html += "</div>";
 
-		if (typeof btnCancel !== "undefined") {
-			btnCancel.addEventListener("click", function () {
-				if (typeof callback === "function") { callback(false); }
-				hideDialog();
-			}, false);
-		}
-	};
+			switch (type) {
+				case "confirm":
+				case "prompt":
+					html = html.replace("{{buttons}}", dialogs.buttons.cancel + dialogs.buttons.ok);
+					break;
+				case "alert":
+					html = html.replace("{{buttons}}", dialogs.buttons.ok);
+					break;
+				default:
+					break;
+			}
 
-	WhatUp.prototype.alert = function (message, fn) {
-		if (typeof fn === "function") { callback = fn; }
-		showDialog(message, alertHTML);
-	};
+			element.className = "whatup wu-" + type;
+			cover.className   = "wu-cover";
+			return html;
+		};
 
-	WhatUp.prototype.confirm = function (message, fn) {
-		if (typeof fn === "function") { callback = fn; }
-		showDialog(message, confirmHTML);
-	};
+		/**
+		 * Hide the dialog and rest to defaults
+		 */
+		hide = function () {
+			callback = null;
+			element.className = "whatup wu-hidden";
+			cover.className   = "wu-cover wu-hidden";
+			element.innerHTML = "";
+		};
 
-	WhatUp.prototype.log = function (message) {
-		showDialog(message, logHTML);
-		// NEEDS TO CLOSE ON A TIMER
-	};
+		/**
+		 * Set the proper button click events
+		 */
+		setEvents = function () {
+			var btnOK     = document.getElementById("wuOK") || undefined,
+			    btnCancel = document.getElementById("wuCancel") || undefined;
 
-	WhatUp.prototype.prompt = function (message, fn) {
-		if (typeof fn === "function") { callback = fn; }
-		showDialog(message, promptHTML);
-		// NEEDS TO PASS THE ENTERED VALUE
-	};
+			if (typeof btnOK !== "undefined") {
+				btnOK.addEventListener("click", function () {
+					if (typeof callback === "function") { callback(true); }
+					hide();
+				}, false);
+			}
 
-	global.whatup = new WhatUp();
+			if (typeof btnCancel !== "undefined") {
+				btnCancel.addEventListener("click", function () {
+					if (typeof callback === "function") { callback(false); }
+					hide();
+				}, false);
+			}
+		};
+
+		/**
+		 * Create an alert dialog box
+		 * 
+		 * @param  {String}   message The message passed from the function
+		 * @param  {Function} fn      [Optional] Callback function
+		 */
+		alert = function (message, fn) {
+			if (typeof fn === "function") { callback = fn; }
+			element.innerHTML = build("alert", message);
+			setEvents();
+		};
+
+		/**
+		 * Create a confirm dialog box
+		 * 
+		 * @param  {String}   message The message passed from the function
+		 * @param  {Function} fn      [Optional] Callback function
+		 */
+		confirm = function (message, fn) {
+			if (typeof fn === "function") { callback = fn; }
+			element.innerHTML = build("confirm", message);
+			setEvents();
+		};
+
+		log = function (message) {
+			// NEEDS TO CLOSE ON A TIMER
+		};
+
+		/**
+		 * Create a prompt dialog box
+		 * 
+		 * @param  {String}   message The message passed from the function
+		 * @param  {Function} fn      [Optional] Callback function
+		 */
+		prompt = function (message, fn) {
+			if (typeof fn === "function") { callback = fn; }
+			element.innerHTML = build("prompt", message);
+			setEvents();
+			// NEEDS TO PASS THE ENTERED VALUE
+		};
+
+		// Bootstrap
+		init();
+
+		return {
+			alert   : alert,
+			confirm : confirm,
+			log     : log,
+			prompt  : prompt
+		};
+	});
+
+	if (typeof global.whatup === "undefined") { global.whatup = WhatUp(); }
 }(this));
